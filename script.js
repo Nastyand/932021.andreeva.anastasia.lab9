4d383ef372c393d1cd89d6bc52ccf0a5c4e3d1d6
Anastasia Andreeva <125570011+Nastyand@users.noreply.github.com>
2023-11-18 20:30:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+let is_equal = false;
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelectorAll("button[class *='btn_']").forEach(element =>{
+        element.addEventListener('click',function(element){
+            if(is_equal)
+            {
+                document.querySelector('input').value = "";
+                document.querySelector('.gray').textContent = "";
+                is_equal = false;
+            }
+            if(isNaN(parseFloat(document.querySelector('input').value.slice(-1))) == true && document.querySelector('input').value.slice(-1) != '.')
+            {
+            document.querySelector('.gray').textContent = "";
+            }
+            document.querySelector('input').value+=element.target.textContent;
+            document.querySelector('.gray').textContent+=element.target.textContent;
+        })
+    });
+    
+
+    document.querySelector('.dot').addEventListener('click', function(){
+        if(document.querySelector('input').value.slice(-1) != '.')
+        {
+            document.querySelector('input').value+= '.';
+            document.querySelector('.gray').textContent+='.'
+        }
+    })
+
+
+    document.querySelector('.delete').addEventListener('click',function(){
+        document.querySelector('input').value = "";
+        document.querySelector('.gray').textContent = "";
+    })
+
+
+    document.querySelector('.left').addEventListener('click', function(){
+        document.querySelector('input').value = document.querySelector('input').value.slice(0,-1);
+        document.querySelector('.gray').textContent=document.querySelector('.gray').textContent.slice(0,-1);
+    })
+
+
+    document.querySelectorAll('.operation').forEach(element =>{
+        element.addEventListener('click', function(element){
+            if (is_equal){is_equal = false;}
+            if(document.querySelector('input').value.length!=0 ){
+                if(isNaN(parseFloat(document.querySelector('input').value.slice(-1))) == 1)
+                {
+                    document.querySelector('input').value = document.querySelector('input').value.slice(0,-1);
+                    document.querySelector('.gray').textContent =document.querySelector('.gray').textContent.slice(0,-1);
+                }
+                document.querySelector('.gray').textContent+=element.target.textContent;
+                document.querySelector('input').value+=element.target.textContent;
+            }
+        });
+    })
+    
+
+    document.querySelector('.equals').addEventListener('click',function(){
+        if(isNaN(parseFloat(document.querySelector('input').value.slice(-1))) == 0)
+        {
+            document.querySelector('input').value = eval(document.querySelector('input').value);
+            document.querySelector('.gray').textContent = document.querySelector('input').value;
+            is_equal = true
+        }
+        
+    })
+})
